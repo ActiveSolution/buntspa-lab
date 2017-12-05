a73b5db7ca56408e772d16d66a943d92f1cbd60c
Oskar Dillén <oskar.dillen@activesolution.se>
2017-12-05 11:24:35 +0100

    Remove some default template code

--- Bunt.Web/ClientApp/app/app.shared.module.ts
+++ Bunt.Web/ClientApp/app/app.shared.module.ts
@@ -6,18 +6,12 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { BuntComponent } from './components/bunt/bunt.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent,
         BuntComponent
     ],
     imports: [
@@ -26,10 +20,7 @@ import { BuntComponent } from './components/bunt/bunt.component';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
             { path: 'bunt', component: BuntComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
